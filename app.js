e32c32f001e07afff05035d3c9bd9d171ec1dad5
Hoang Manh Dung <manhdung18102003@gmail.com>
2022-08-27 17:13:21 +0700

    Update port heroku

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ server.use((req, res, next) => {
 })
 
 // Use default router
+const PORT = process.env.PORT || 3000;
 server.use(router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('JSON Server is running')
 }) 
